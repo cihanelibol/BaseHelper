8689bcede2c6e45a96157b02f8df9b5fb9a90185
cihanelibol <cihanelibol47@gmail.com>
2024-09-28 20:54:44 +0300

    Add project files.

---
+++ BaseHelper/Helper/RestRequest.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Text.Json;
+
+public class RestRequest<TResponse>
+{
+    private readonly HttpClient _httpClient;
+
+    public RestRequest(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<TResponse> GetAsync(string url)
+    {
+        var response = await _httpClient.GetAsync(url);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Error: {response.StatusCode}, {response.ReasonPhrase}");
+        }
+
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<TResponse>(content);
+    }
+
+    public async Task<TResponse> PostAsync<TRequest>(string url, TRequest requestData)
+    {
+        var jsonContent = new StringContent(JsonSerializer.Serialize(requestData), Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync(url, jsonContent);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Error: {response.StatusCode}, {response.ReasonPhrase}");
+        }
+
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<TResponse>(content);
+    }
+
+    public async Task<TResponse> PutAsync<TRequest>(string url, TRequest requestData)
+    {
+        var jsonContent = new StringContent(JsonSerializer.Serialize(requestData), Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PutAsync(url, jsonContent);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Error: {response.StatusCode}, {response.ReasonPhrase}");
+        }
+
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<TResponse>(content);
+    }
+
+    public async Task<bool> DeleteAsync(string url)
+    {
+        var response = await _httpClient.DeleteAsync(url);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Error: {response.StatusCode}, {response.ReasonPhrase}");
+        }
+
+        return response.IsSuccessStatusCode;
+    }
+}
